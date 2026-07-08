a1583c8527c97218c8b0bc48e0206e0dd78a3bd5
Akash Dey <145528736+akash6270@users.noreply.github.com>
2026-07-08 13:36:52 +0530

    Fix formatting and structure of script.js

--- script.js
+++ script.js
@@ -11,6 +11,20 @@ buttons.forEach((button) => {
         }
        catch{
         display.value="ERROR";
+         public class RandomCode {
+    public static void main(String[] args) {
+        int n1 = 0, n2 = 1;
+
+        System.out.println("First 10 Fibonacci Numbers:");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(n1 + " ");
+            int next = n1 + n2;
+            n1 = n2;
+            n2 = next;
+        }
+    }
+}
        }
     }
     else if(e.target.innerText==='AC'){
